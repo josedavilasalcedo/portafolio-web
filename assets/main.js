9a4954d53359ed52942bbd61bc7c0175536cc1a5
Jose Davila <josedavilasalcedo97@gmail.com>
2024-06-17 20:21:05 -0500

    se agrego funcionalidad al boton de copiar el correo

--- assets/main.js
+++ assets/main.js
@@ -7,3 +7,15 @@ document.addEventListener('DOMContentLoaded', function() {
     menuToggle.classList.toggle('open');
   });
 });
+
+function copyEmail() {
+  // Selecciona el elemento que contiene el correo electrónico
+  const emailElement = document.querySelector('.hero__contact-email');
+  const email = emailElement.textContent;
+
+  navigator.clipboard.writeText(email).then(() => {
+    alert('Correo electrónico copiado al portapapeles');
+  }).catch(err => {
+    console.error('Error al copiar el correo electrónico: ', err);
+  });
+}
